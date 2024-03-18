f54dc4b10bef31b8b07fe96e47a80f5d4f2946c3
Saba Kia <skia6589@gmail.com>
2024-03-18 15:13:02 +0000

    created submit event (logic for adding new list item)

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,10 +1,15 @@
 const toDoForm = document.querySelector("#todo-form");
 const userInput = document.querySelector("#new-todo");
+const toDoList = document.querySelector("#list");
 
 userInput.addEventListener("input", (evt) => {
     console.log(evt);
 });
 
-toDoForm.addEventListener("submit", () => {
-
+toDoForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const newListItem = document.createElement("li");
+    newListItem.innerText = evt.target.value;
+    toDoList.appendChild(newListItem);
 });
+
