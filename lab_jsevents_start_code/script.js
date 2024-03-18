471e22bff429a21f8baf6bc19d9c31aca6899dde
Saba Kia <skia6589@gmail.com>
2024-03-18 14:47:31 +0000

    created event listener for input

---
+++ lab_jsevents_start_code/script.js
@@ -0,0 +1,10 @@
+const toDoForm = document.querySelector("#todo-form");
+const userInput = document.querySelector("#new-todo");
+
+userInput.addEventListener("input", (evt) => {
+    console.log(evt);
+});
+
+toDoForm.addEventListener("submit", () => {
+
+});
